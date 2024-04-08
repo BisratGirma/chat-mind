69f565c28b830e56bbbcce5cfdf94dc13c4914f7
Bisrat Girma <bisratx@gmail.com>
2024-04-08 09:42:26 +0300

    text splitter

--- app.mjs
+++ app.mjs
@@ -1,8 +1,19 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 try {
-    const result = await fetch('./data.txt')
-    const text = await result.text()
+  const result = await fetch(
+    "https://raw.githubusercontent.com/BisratGirma/chat-mind/main/data.txt"
+  );
+  const text = await result.text();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+  });
+
+  const output = await splitter.createDocuments([text]);
+
+  // splitted text
+  console.log(output);
 } catch (err) {
-    console.log(err)
+  console.log(err);
 }
---
+++ output.md
@@ -0,0 +1,4 @@
+{
+pageContent: "Main Faqs what programming l...",
+metadata: { loc: { lines: { from: 1, to: 14 }} }
+}
